71e14fbeeb429eae91cc47c8998afdd46cffd1fb
dansun2 <dansunmusik7@gmail.com>
2024-06-03 12:52:40 +0900

    240603

--- 07_props/src/components/02_propsDrilling/DrillingParents.vue
+++ 07_props/src/components/02_propsDrilling/DrillingParents.vue
@@ -1,3 +1,22 @@
 <script setup>
+    import { provide, ref } from 'vue';
 
+    const count = ref(0);
+    provide('count', count);
 </script>
+
+<template>
+    <div class="container">
+        <h1>parents : {{ count }}</h1>
+        <Child />
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        border: 1px solid;
+       display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
